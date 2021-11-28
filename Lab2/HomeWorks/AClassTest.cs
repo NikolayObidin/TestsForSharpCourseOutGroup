86f3fcd5b52bcb0131cbc4e70af7bb68870667bf
Your-Full-Name <innot.808@gmail.com>
2021-11-28 16:10:33 +0300

    Lab2 cmpltd

--- Lab2/HomeWorks/AClassTest.cs
+++ Lab2/HomeWorks/AClassTest.cs
@@ -1,8 +1,14 @@
 namespace Lab2.HomeWorks
 {
 
-    public abstract class AClassTest
+    public abstract class AClassTest 
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public AClassTest(string name, int age) 
+        {
+            Name = name; Age = age;
+        }
         public abstract string Print();
     }
 }
--- Lab2/HomeWorks/ClassTest1.cs
+++ Lab2/HomeWorks/ClassTest1.cs
@@ -1,10 +1,19 @@
 namespace Lab2.HomeWorks
 {
-    public class ClassTest1
-    {
-        public string Print()
+    public class ClassTest1 : AClassTest, Interface
+    {    
+        public ClassTest1(string name, int age)
+            :base(name, age)
         {
+        }
+        public override string Print()
+        { 
             return "It is ClassTest1";
+        }
+
+        string Interface.PrintFromInterface()
+        {
+            return "It is ClassTest1Interface";
         }
     }
 }
